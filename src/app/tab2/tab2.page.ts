5fdfa1cfd5bc7f3737ef5fc7290a0c6bbea56b29
santiago.larsen <santiagolarsen@ultharsoftware.com>
2020-05-30 11:50:05 -0300

    Mapa de google, settings de Firebase

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { Plugins } from "@capacitor/core";
+const { Geolocation } = Plugins;
 
+declare var google;
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+	selector: "app-tab2",
+	templateUrl: "tab2.page.html",
+	styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+	// Map related
+	@ViewChild("map", { static: false }) mapElement: ElementRef;
+	map: any;
+	markers = [];
 
-  constructor() {}
+	constructor() {}
 
+	ionViewWillEnter() {
+		this.loadMap();
+	}
+	loadMap() {
+		let latLng = new google.maps.LatLng(-38.55, -55.6673267);
+
+		let mapOptions = {
+			center: latLng,
+			zoom: 5,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			disableDefaultUI: true,
+		};
+
+		this.map = new google.maps.Map(
+			this.mapElement.nativeElement,
+			mapOptions
+		);
+	}
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebaseConfig: {
+		apiKey: "AIzaSyCeghIrlrjLM4Suiy9BHet4CBoas3-dUo0",
+		authDomain: "walksafe-bceb3.firebaseapp.com",
+		databaseURL: "https://walksafe-bceb3.firebaseio.com",
+		projectId: "walksafe-bceb3",
+		storageBucket: "walksafe-bceb3.appspot.com",
+		messagingSenderId: "131053567300",
+		appId: "1:131053567300:web:b53051145414cfd460ab11",
+		measurementId: "G-2VFVJFH9NC",
+	},
 };
 
 /*
