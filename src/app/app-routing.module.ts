afa34ec1186b288ebb564bf226819540d9a11ea3
Leo <35300461+frexel@users.noreply.github.com>
2020-05-30 21:02:58 -0300

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
-    path: '',
+    path: 'app',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 @NgModule({
   imports: [
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { AngularFirestoreCollection } from "@angular/fire/firestore/public_api";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
+})
+export class LoginPage implements OnInit {
+  user;
+  usersCollection: AngularFirestoreCollection;
+
+  constructor(
+    private router: Router,
+    private afauth: AngularFireAuth,
+    private afs: AngularFirestore
+  ) {}
+
+  ngOnInit() {}
+
+  login() {
+    this.afauth.signInAnonymously().then((res) => {
+      this.usersCollection = this.afs.collection(`users`);
+      this.user = res.user;
+      let userRef = this.usersCollection.doc(this.user.uid);
+      userRef.set({
+        position: {
+          lat: "",
+          lng: "",
+        },
+        timestamp: 0,
+        notification: "",
+      });
+      this.router.navigate(["/app"]);
+    });
+  }
+}
