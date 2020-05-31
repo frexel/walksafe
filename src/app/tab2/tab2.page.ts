a52e056324c2ada2ff47ca6f667789fcbcb65af6
Leo <35300461+frexel@users.noreply.github.com>
2020-05-31 04:07:25 -0300

    error fix

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,132 +3,141 @@ import { Plugins } from "@capacitor/core";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import {
-	AngularFirestoreCollection,
-	AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestore,
 } from "@angular/fire/firestore";
 import { AuthService } from "../services/auth.service";
 const { Geolocation } = Plugins;
 
 declare var google;
 @Component({
-	selector: "app-tab2",
-	templateUrl: "tab2.page.html",
-	styleUrls: ["tab2.page.scss"],
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
-	@ViewChild("map", { static: false }) mapElement: ElementRef;
-	user = null;
-	userid;
-	map: any;
-	markers = [];
-	positions: Observable<any>;
-	usersCollection: AngularFirestoreCollection<any>;
-	userDoc;
-	isTracking = false;
-	watch: string;
+  @ViewChild("map", { static: false }) mapElement: ElementRef;
+  user = null;
+  userid;
+  map: any;
+  markers = [];
+  positions: Observable<any>;
+  usersCollection: AngularFirestoreCollection<any>;
+  userDoc;
+  isTracking = false;
+  watch: string;
+  lat;
+  lng;
 
-	constructor(
-		private afs: AngularFirestore,
-		private authservice: AuthService
-	) {
-		this.usersCollection = this.afs.collection(`users`);
-		/* this.user = this.authservice.getuserAuth().subscribe(user => {
+  constructor(private afs: AngularFirestore, private authservice: AuthService) {
+    this.usersCollection = this.afs.collection(`users`);
+    /* this.user = this.authservice.getuserAuth().subscribe(user => {
 			this.userid = user.uid;
 		  }); */
-		this.userDoc = this.usersCollection.doc("2");
+    this.userDoc = this.usersCollection.doc("2");
 
-		this.getMyPosition();
-	}
+    this.getMyPosition();
+  }
 
-	ionViewWillEnter() {
-		this.loadMap();
-	}
+  ionViewWillEnter() {
+    this.loadMap();
+  }
 
-	// conseguir mi posicion y actualizar la DB
-	getMyPosition() {
-		//this.userDoc = this.usersCollection.doc(this.user);
-		this.positions = this.userDoc
-			.snapshotChanges()
-			.pipe(map((doc) => (doc as any).payload.data().position));
-		// actualizar en cada cambio
-		this.positions.subscribe((positions) => {
-			this.updateMap(positions);
-		});
-	}
+  // conseguir mi posicion y actualizar la DB
+  getMyPosition() {
+    //this.userDoc = this.usersCollection.doc(this.user);
+    this.positions = this.userDoc
+      .snapshotChanges()
+      .pipe(map((doc) => (doc as any).payload.data().position));
+    // actualizar en cada cambio
+    this.positions.subscribe((positions) => {
+      this.updateMap(positions);
+    });
+  }
 
-	// inicializar el mapa
-	loadMap() {
-		let latLng = new google.maps.LatLng(-35.9036442, -57.6673267);
+  // inicializar el mapa
+  loadMap() {
+    let latLng = new google.maps.LatLng(-35.9036442, -57.6673267);
 
-		let mapOptions = {
-			center: latLng,
-			zoom: 5,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			disableDefaultUI: true,
-		};
+    let mapOptions = {
+      center: latLng,
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
+    };
 
-		this.map = new google.maps.Map(
-			this.mapElement.nativeElement,
-			mapOptions
-		);
-	}
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
 
-	// empezar a trackear
-	startTracking() {
-		this.isTracking = true;
-		this.watch = Geolocation.watchPosition({}, (position, err) => {
-			if (position) {
-				this.addNewPosition(
-					position.coords.latitude,
-					position.coords.longitude,
-					position.timestamp
-				);
-			}
-		});
+  sendAlert() {
+    this.userDoc.set({
+      notifications: "panic",
+      state: "sharing",
+      position: {
+        lat: this.lat,
+        lng: this.lng,
+      },
+    });
+  }
 
-		this.positions.subscribe((positions) => {
-			this.updateMap(positions);
-		});
-	}
+  // empezar a trackear
+  startTracking() {
+    this.isTracking = true;
+    this.watch = Geolocation.watchPosition({}, (position, err) => {
+      if (position) {
+		this.lat = position.coords.latitude;
+		this.lng = position.coords.longitude;
+        this.addNewPosition(
+          position.coords.latitude,
+          position.coords.longitude,
+          position.timestamp
+        );
+      }
+    });
 
-	// cerrar la suscripción
-	stopTracking() {
-		Geolocation.clearWatch({ id: this.watch }).then(() => {
-			this.isTracking = false;
-		});
-		this.userDoc.set({
-			notification: "stopped",
-		});
-	}
+    this.positions.subscribe((positions) => {
+      this.updateMap(positions);
+    });
+  }
 
-	// guardar la ubicacion y actualizar el mapa
-	addNewPosition(lat, lng, timestamp) {
-		this.userDoc.set({
-			position: {
-				lat,
-				lng,
-			},
-			timestamp,
-			state: "sharing",
-		});
+  // cerrar la suscripción
+  stopTracking() {
+    Geolocation.clearWatch({ id: this.watch }).then(() => {
+      this.isTracking = false;
+    });
+    this.userDoc.set({
+      notification: "stopped",
+    });
+  }
 
-		let position = new google.maps.LatLng(lat, lng);
-		this.map.setCenter(position);
-		this.map.setZoom(5);
-	}
+  // guardar la ubicacion y actualizar el mapa
+  addNewPosition(lat, lng, timestamp) {
+    this.userDoc.set({
+      position: {
+        lat,
+        lng,
+      },
+      timestamp,
+      state: "sharing",
+    });
 
-	updateMap(position) {
-		this.markers.map((marker) => marker.setMap(null));
-		this.markers = [];
+    let position = new google.maps.LatLng(lat, lng);
+    this.map.setCenter(position);
+    this.map.setZoom(5);
+  }
 
-		let latLng = new google.maps.LatLng(position.lat, position.lng);
+  updateMap(position) {
+    this.markers.map((marker) => marker.setMap(null));
+    this.markers = [];
 
-		let marker = new google.maps.Marker({
-			map: this.map,
-			animation: google.maps.Animation.DROP,
-			position: latLng,
-		});
-		//console.log(this.markers, marker);
-		this.markers.push(marker);
-	}
+    let latLng = new google.maps.LatLng(position.lat, position.lng);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: latLng,
+    });
+    //console.log(this.markers, marker);
+    this.markers.push(marker);
+  }
 }
